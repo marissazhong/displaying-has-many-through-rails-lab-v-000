1c42cc46139971d5ff8b6e5f9b20ec2c96c4cddb
Marissa Zhong <mwz2101@columbia.edu>
2018-07-26 19:12:22 +0000

    automatically backed up by learn

--- db/migrate/20180726163651_create_appointments.rb
+++ db/migrate/20180726163651_create_appointments.rb
@@ -4,6 +4,7 @@ class CreateAppointments < ActiveRecord::Migration
       t.datetime :appointment_datetime
       t.belongs_to :doctor
       t.belongs_to :patient
+      t.timestamps null: false
     end
   end
 end
